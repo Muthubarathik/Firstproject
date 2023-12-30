f9a601664fa8e23e6507be621df77b44ee5367d3
muthubarathik <muthubarathi.mk@gmail.com>
2023-12-30 20:17:40 +0530

    data types

--- src/Printstatement/Helloworld.java
+++ src/Printstatement/Helloworld.java
@@ -1,10 +1,38 @@
 package Printstatement;
 
+import java.util.Scanner;
+
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    System.out.println("Hello World");
+		
+		//Data types
+		byte num1=127;
+		short num2=32000;
+		int num3=545454;
+		long num4=3453453L;
+		float num5=0.454f;
+		double num6=0.34454354;
+		char letter='B';
+		Boolean var=true;
+		System.out.println("byte : "+num1+"\n" +
+				"short : "+num2+"\n" +
+				"int : "+num3+  "\n" +
+				"long : "+num4+ "\n" +
+				"float : "+num5+"\n" +
+				"double : "+num6+"\n" +
+				"char : "+letter+"\n" +
+				"Boolean : "+var);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+  
 	}
 
 }
